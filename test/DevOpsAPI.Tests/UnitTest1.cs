2a8f5983afbdb782d25ff9bf73072fd961fac2c9
Marcelo Jose Macedo <marcelomacedo1980@hotmail.com>
2019-06-15 09:25:08 +0800

    Initial Commit

---
+++ test/DevOpsAPI.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using DevOpsAPI.Controllers;
+
+namespace DevOpsAPI.Tests
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+
+        [Fact]
+        public void Test1()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("marcelo", returnValue.Value);
+
+        }
+    }
+}
